6a886036ef2d47c206b5840fc24aa7739ef72a17
mohammad-aarif <arifbd354@gmail.com>
2022-01-07 15:25:24 +0600

    add delete button on meal log

--- src/Pages/Dashboard/UsersDashboard/Manager/AddMeal/MealLog.js
+++ src/Pages/Dashboard/UsersDashboard/Manager/AddMeal/MealLog.js
@@ -7,16 +7,18 @@ const MealLog = () => {
     return (
         <div className='mx-auto py-5'>
                 <div className="add-container general-btn my-3 px-3 py-2 row justify-content-between align-items-center">
-                    <p className='fw-bold col-4'>Date</p>
-                    <p className='fw-bold text-center col-4'>Name</p>
-                    <p className='fw-bold col-4 text-end'>Meal</p>
+                    <p className='fw-bold col-3'>Date</p>
+                    <p className='fw-bold text-center col-3'>Name</p>
+                    <p className='fw-bold col-3 text-center'>Meal</p>
+                    <p className='fw-bold col-3 text-end'>Delete</p>
                 </div>
             {
                 mealsAdmin.map(data => (
                     <div key={data._id} className="add-container my-3 px-3 py-2 row justify-content-between align-items-center">
-                        <p className='fw-bold col-4'>{data.date}</p>
-                        <p className='fw-bold text-center col-4'>{data.name}</p>
-                        <p className='fw-bold col-4 text-end'>{data.meal}</p>
+                        <p className='fw-bold col-3'>{data.date}</p>
+                        <p className='fw-bold text-center col-3'>{data.name}</p>
+                        <p className='fw-bold col-3 text-end'>{data.meal}</p>
+                        <button className='btn col-3 btn-remove'>Delete</button>
                     </div>
                 ))
             }
